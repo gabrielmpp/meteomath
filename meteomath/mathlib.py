a76c8e7c5e4188bcb1a1d078f244482c32bf89f3
Gabriel Martins Palma Perez <gmpp@jasmin-sci4.ceda.ac.uk>
2019-05-02 13:56:28 +0100

    update

--- meteomath/mathlib.py
+++ meteomath/mathlib.py
@@ -1,14 +1,30 @@
 import xarray as xr
 import numpy as np
 
-def vertical_div_from_flux(zonal_flux, meridional_flux, vertical_coord = 'pressure'):
+def discrete_vertical_integral(zonal_flux, meridional_flux, f, coord = 'pressure'):
+    '''
+    Method to compute the discrete integral of an array over a given coordinateself.
 
-    vertical_deltas = zonal_flux[vertical_coord].diff(vertical_coord)
-    div = divergence(zonal_flux, meridional_flux)
-    div = div.interp({vertical_coord: pressure_deltas[vertical_coord]})
+    Keyword arguments:
+    zonal_flux -- xarray DataArray zonal wind speed or flux
+    meridional_flux -- xarray DataArray meridional wind speed or flux
+    f -- function to apply before the vertical integration. Examples: meteomath.divergence or meteomath.vorticity
+    coord - coordinate to integrate upon
+    '''
+
+    if not isinstance(zonal_flux, xr.DataArray) or isinstance(meridional_flux, xr.DataArray):
+        raise ValueError('The inputs should be xarray dataarray')
+
+    if zonal_flux[vertical_coord] != meridional_flux[vertical_coord]:
+        raise ValueError('The vertical coordinates should match')
+
+    vertical_deltas = zonal_flux[vertical_coord].diff(coord)
+    div = f(zonal_flux, meridional_flux)
+    div = div.interp({vertical_coord: pressure_deltas[coord]})
     div = div*vertical_deltas
-    div_int = div.sum(vertical_coord)
+    div_int = div.sum(coord)
     return div_int
+
 
 def to_cartesian(array, lon_name = 'longitude', lat_name = 'latitude', earth_r = 6371000):
 
