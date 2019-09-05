7436c4124b259b2838bd413c7fc83ef75be75ef2
gabriel <gabrielmpp2@gmail.com>
2019-09-05 14:54:51 +0100

    docs

--- source/conf.py
+++ source/conf.py
@@ -24,7 +24,7 @@ author = 'meteomath'
 # The full version, including alpha/beta/rc tags
 release = '1.0'
 
-
+master_doc = 'index'
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
