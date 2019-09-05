894d77a8f2ff31066db73686dd47f1e4e815cce6
gabriel <gabrielmpp2@gmail.com>
2019-09-05 14:20:47 +0100

    included stagger functions

--- meteomath/__init__.py
+++ meteomath/__init__.py
@@ -1,1 +1,2 @@
-from meteomath.mathlib import to_cartesian, divergence, vorticity, strain_rate, discrete_vertical_integral
+from meteomath.mathlib import to_cartesian, divergence, vorticity, strain_rate, discrete_vertical_integral, \
+    interpolate_c_stagger, _assert_regular_latlon_grid
